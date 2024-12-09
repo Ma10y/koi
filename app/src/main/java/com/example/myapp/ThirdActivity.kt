8dc6b87fc370fc96c6c2b83a25a51990b8776d0e
mallo <kamataabuser@gmail.com>
2024-12-10 00:43:47 +1100

    s

---
+++ app/src/main/java/com/example/myapp/ThirdActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_third)
+
+        val button: Button = findViewById(R.id.button7)
+        button.setOnClickListener {
+            val intent = Intent(this, SummaryActivity::class.java)
+            startActivity(intent) }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
